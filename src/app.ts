37b2e247b96dfec2d9f592c1475e34767d99361f
brianmak3 <brianworkss@gmail.com>
2023-09-26 20:53:58 +0300

    Fix: new

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,7 @@ import Controllers from './Controllers'
 import { onRun, socketBroadCast } from "./helper";
 import Sockets from './sockets'
 import {  verifyJWT } from "./helper/auth";
-
+process.env.AWS_SDK_JS_SUPPRESS_MAINTENANCE_MODE_MESSAGE = '1';
 
 const port: number = parseInt(process.env.port as string);
 const app: Application = express(),
@@ -25,13 +25,12 @@ app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 app.use('/', express.static(path.join(__dirname, process.env.env === 'dev' ? '../dist/public' : '../public')));
 
-//configure the JWT middleware
+
 
 app.use(verifyJWT);
 //checking server side connection
 Sockets({ http, clients })
 app.post("/", (req, res) => {
-    console.log(req.body)
     try {
         (Controllers as any)[req.body.controller]({
             data: req.body,
